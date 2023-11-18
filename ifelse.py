f5f1d85056d361656705f9dbb6c9dcd2830617b4
callmelucky <publiccamelucky@gmail.com>
2023-11-18 14:16:09 +0530

    IFPRO

---
+++ ifelse.py
@@ -0,0 +1,21 @@
+# W.a.p.to show A person vot or not vot
+'''Age = int(input("Enter your Age :"))
+if Age>=18:
+    print("You Are voter")
+else :
+    print("You Are NOt voter") 
+    
+   # W.a.P. to show user input five subject mark result pass or fail use if else condition
+mar = int (input("Enter Your marathi subject mark")) 
+hin = int (input("Enter Your Hindi subject mark")) 
+eng = int (input("Enter Your English subject mark")) 
+bio = int (input("Enter Your bio subject mark")) 
+math = int (input("Enter Your math subject mark")) 
+if mar>=35 and hin>=35 and eng>=35 and bio>=35 and math>=35 :
+    print("You are Pass in all subject")
+    
+else:
+    print("You are a fail")
+    '''
+    
+    
---
+++ ifpro.py
@@ -0,0 +1,34 @@
+'''age = int(input("Enter your age"))
+if age>18:
+    print("You are voter")
+    
+age = int(input("Enter your age"))
+if age<18:
+    print("You are not voter")
+    
+    #W.a.p.to show number is negativi
+num = int(input("Enter your number"))
+if num<0:
+    print("your number is negativi ")
+    
+    # W.a p. to show a number is odd
+no = int (input("Enter your number "))
+if no%2!=0:
+    print("This number is odd")
+
+ # W.a p. to show a number is positive
+no = int (input("Enter your number "))
+if no>0:
+    print("This number is positive")
+    
+
+ # W.a p. to show a number is even
+no = int (input("Enter your number "))
+if no%2==0:
+    print("This number is even")
+else:
+    print("This number is odd")  
+    '''
+    
+    
+    
